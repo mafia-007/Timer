550b044b492cb25c110a4995d5faa8f77b544b98
Samuel <clooner0007@gmail.com>
2017-03-26 02:41:17 +0430

    Update tools.js

--- tools.js
+++ tools.js
@@ -1,25 +1,4 @@
-module.exports = {
-  Timer: function (callback, delay) {
-      var id, started, remaining = delay, running
-
-      this.start = function() {
-          running = true
-          started = new Date()
-          id = setTimeout(callback, remaining)
-      }
-
-      this.pause = function() {
-          running = false
-          clearTimeout(id)
-          remaining -= new Date() - started
-      }
-
-      this.getTimeLeft = function() {
-          if (running) {
-              this.pause()
-              this.start()
-          }
-
+function()
           return remaining
       }
 
